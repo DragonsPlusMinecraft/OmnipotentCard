351f7b59c377fb0ee3482a4f6f5ff3e0977cc995
Kasualix <670067575@qq.com>
2023-05-08 03:53:19 +0800

    Compatible with new version of Forge

--- src/main/java/plus/dragons/omnicard/event/ForgeBusEvent.java
+++ src/main/java/plus/dragons/omnicard/event/ForgeBusEvent.java
@@ -1,5 +1,6 @@
 package plus.dragons.omnicard.event;
 
+import net.minecraftforge.event.entity.living.MobSpawnEvent;
 import plus.dragons.omnicard.blockentity.SpecialCardBlockTileEntity;
 import plus.dragons.omnicard.card.BlockCards;
 import plus.dragons.omnicard.misc.MiscUtil;
@@ -8,25 +9,23 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.event.entity.living.LivingSpawnEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ForgeBusEvent {
     @SubscribeEvent
-    public static void preventHostileNearSEALCard(LivingSpawnEvent.CheckSpawn event) {
+    public static void preventHostileNearSEALCard(MobSpawnEvent.FinalizeSpawn event) {
         // It only stops natural spawning
-        if (event.getSpawnReason() == MobSpawnType.NATURAL) {
+        if (event.getSpawnType() == MobSpawnType.NATURAL) {
             if (MiscUtil.isHostile(event.getEntity(), false)) {
                 for (BlockPos pos : BlockPos.betweenClosed(new BlockPos((int) (event.getX() - 8), (int) event.getY() - 8, (int) (event.getZ() - 8)), new BlockPos((int) (event.getX() + 8), (int) (event.getY() + 8), (int) (event.getZ() + 8)))) {
                     BlockState blockState = event.getLevel().getBlockState(pos);
                     if (blockState.getBlock() == BlockRegistry.SPECIAL_CARD_BLOCK.get()) {
                         BlockEntity tileEntity = event.getLevel().getBlockEntity(pos);
-                        if (tileEntity instanceof SpecialCardBlockTileEntity) {
-                            SpecialCardBlockTileEntity specialCardBlockTile = (SpecialCardBlockTileEntity) tileEntity;
+                        if (tileEntity instanceof SpecialCardBlockTileEntity specialCardBlockTile) {
                             if (specialCardBlockTile.getCardType() == BlockCards.SEAL) {
-                                event.setResult(net.minecraftforge.eventbus.api.Event.Result.DENY);
+                                event.setSpawnCancelled(true);
                             }
                         }
                     }
